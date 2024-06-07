62990a80adfb14cd903054400722608eaa79f1a9
tonmoy <tonmoy3673@gmail.com>
2024-06-07 18:13:57 +0600

    :boom: set get and post api

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const app = express()
-require("dotenv").config();
-const port = 5000
-const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const port = 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodie.v0mnxbq.mongodb.net/?retryWrites=true&w=majority&appName=foodie`;
 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodie.wcw1hm1.mongodb.net/?retryWrites=true&w=majority&appName=foodie`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,34 +19,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    const allFoods = client.db("allFoods");
-    const foodCollection = allFoods.collection("foodCollection");
-    app.post('/foods',async(req,res)=>{
-        const foodData=req.body;
-        const result=await foodCollection.insertOne(foodData)
-        res.send(result);
+    const foodDB = client.db("foodDB");
+    const foodCollection = foodDB.collection("foodCollection");
+    
+    app.get('/foods',async(req,res)=>{
+      const foods=await foodCollection.find({}).toArray();
+      res.send(foods);
     })
 
-    app.get('/foods',async(req,res)=>{
-        
-        const result=await foodCollection.find();
-        res.send(result);
-        console.log('MongoDB is Connected');
+    app.post('/foods',async(req,res)=>{
+      const foods={
+        name:'Noodles',
+        price:400,
+      }
+      const result= await foodCollection.insertOne(foods);
+      res.send()
     })
-    
+
+    app.get('/foods/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const food= await foodCollection.findOne(query);
+      res.send(food);
+    })
+
+    console.log("MongoDB Connected!!");
   } finally {
-  
+    
+    
   }
 }
 run().catch(console.dir);
 
 
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server Connected')
 })
 
 app.listen(port, () => {
